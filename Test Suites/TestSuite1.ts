56053e59f119539e2a026afd362e20942a392d05
Avik Sarkar <Avik Sarkar@LAPTOP-BCB5STRI>
2024-04-06 11:39:05 +0530

    TestSuite

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>40d5c2bc-97cd-4ff1-8721-f06fbbada824</testSuiteGuid>
+</TestSuiteEntity>
