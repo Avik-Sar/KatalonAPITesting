27367fc987d167ad6b04db857fefb027f5e2e7dd
Avik Sarkar <Avik Sarkar@LAPTOP-BCB5STRI>
2024-04-13 12:53:26 +0530

    Added Test Suit for Visual Testing.

---
+++ Test Suites/Visual_Test-4.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Visual_Test-4</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>79ea5725-b536-4f82-abd5-b7469bdf5350</testSuiteGuid>
+   <testCaseLink>
+      <guid>01fa260d-8f10-4e12-874f-9a4909d2fd48</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Visual_Test/Visual_3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
